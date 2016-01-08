48412d8da1cca80bb0c40d8e1128de06a9004647
rhorton4 <ryland.horton@gmail.com>
2016-01-07 20:35:33 -0800

    Finished for the night

--- CMPE1600/RylandHortonReviewLab/RylandHortonReviewLab/Program.cs
+++ CMPE1600/RylandHortonReviewLab/RylandHortonReviewLab/Program.cs
@@ -13,11 +13,11 @@ namespace RylandHortonReviewLab
         static void Main(string[] args)
         {
             bool runProg = true;
-            double userMoney;
+            decimal userMoney;
             CDrawer window = new CDrawer();
             do
             {
-                double[,] cashAndCoins;
+                decimal[,] cashAndCoins;
                 window.Clear();
                 Console.Clear();
                 Console.WriteLine("\t\tCMPE 1700 Money Calculator\n\n");
@@ -33,9 +33,9 @@ namespace RylandHortonReviewLab
             } while (runProg);
 
         }
-        static public double getMoney()
+        static public decimal getMoney()
         {
-            double retVal = 0;
+            decimal retVal = 0;
             string userVal;
             bool error = false;
 
@@ -47,7 +47,7 @@ namespace RylandHortonReviewLab
                 error = false;
                 try
                 {
-                    retVal = double.Parse(userVal);
+                    retVal = decimal.Parse(userVal);
                 }
                 catch
                 {
@@ -56,45 +56,45 @@ namespace RylandHortonReviewLab
                     Console.WriteLine("\nError. Please enter a valid currency amount.");
                     Console.ResetColor();
                 }
-                if (retVal < 0)
+                if (retVal < 0 || retVal > 9999999)
                 {
                     error = true;
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("\nError. Please enter a valid currency amount.");
                     Console.ResetColor();
                 }
-                retVal = Math.Round(retVal, 2);
             } while (error);
             return retVal;
         }
-        static public double[,] calcMoney(double cashVal)
+        static public decimal[,] calcMoney(decimal cashVal)
         {
-            double[,] currVals = new double[2, 10];
+            decimal[,] currVals = new decimal[2, 10];
             currVals[0, 0] = 50;
             currVals[0, 1] = 20;
             currVals[0, 2] = 10;
             currVals[0, 3] = 5;
             currVals[0, 4] = 2;
             currVals[0, 5] = 1;
-            currVals[0, 6] = 0.25;
-            currVals[0, 7] = 0.1;
-            currVals[0, 8] = 0.05;
-            currVals[0, 9] = 0.01;
-            currVals[1, 0] = (int)cashVal / 50;
-            currVals[1, 1] = (int)cashVal % 50 / 20;
-            currVals[1, 2] = (int)cashVal % 50 % 20 / 10;
-            currVals[1, 3] = (int)cashVal % 50 % 20 % 10 / 5;
-            currVals[1, 4] = (int)cashVal % 50 % 20 % 10 % 5 / 2;
-            currVals[1, 5] = (int)cashVal % 50 % 20 % 10 % 5 % 2 / 1;
-            currVals[1, 6] = Math.Floor(cashVal % 50 % 20 % 10 % 5 % 2 % 1 / 0.25);
-            currVals[1, 7] = Math.Floor(cashVal % 50 % 20 % 10 % 5 % 2 % 1 % 0.25 / 0.10);
-            currVals[1, 8] = Math.Floor(cashVal % 50 % 20 % 10 % 5 % 2 % 1 % 0.25 % 0.10 / 0.05);
-            currVals[1, 9] = Math.Floor(cashVal % 50 % 20 % 10 % 5 % 2 % 1 % 0.25 % 0.10 % 0.05 / 0.01);
+            currVals[0, 6] = 0.25m;
+            currVals[0, 7] = 0.1m;
+            currVals[0, 8] = 0.05m;
+            currVals[0, 9] = 0.01m;
+            currVals[1, 0] = Math.Floor(cashVal / 50m);
+            currVals[1, 1] = Math.Floor(cashVal % 50m / 20m);
+            currVals[1, 2] = Math.Floor(cashVal % 50m % 20m / 10m);
+            currVals[1, 3] = Math.Floor(cashVal % 50m % 20m % 10m / 5m);
+            currVals[1, 4] = Math.Floor(cashVal % 50m % 20m % 10m % 5m / 2m);
+            currVals[1, 5] = Math.Floor(cashVal % 50m % 20m % 10m % 5m % 2m / 1m);
+            currVals[1, 6] = Math.Floor(cashVal % 50m % 20m % 10m % 5m % 2m % 1m / 0.25m);
+            currVals[1, 7] = Math.Floor(cashVal % 50m % 20m % 10m % 5m % 2m % 1m % 0.25m / 0.10m);
+            currVals[1, 8] = Math.Floor(cashVal % 50m % 20m % 10m % 5m % 2m % 1m % 0.25m % 0.10m / 0.05m);
+            currVals[1, 9] = Math.Round(cashVal % 50m % 20m % 10m % 5m % 2 % 1m % 0.25m % 0.10m % 0.05m / 0.01m);
             return currVals;
         }
-        static public void drawMoney(CDrawer window, double cashVal, double[,] currVals)
+        static public void drawMoney(CDrawer window, decimal cashVal, decimal[,] currVals)
         {
-            window.AddText("$" + cashVal.ToString(), 24, 400, 50, 0, 0, Color.Yellow);
+            string cashTotal = string.Format("{0:C}", cashVal);
+            window.AddText(cashTotal, 24, 400, 50, 0, 0, Color.Yellow);
             Color[] colours = { Color.Firebrick, Color.LightGreen, Color.Magenta, Color.LightSkyBlue, Color.Silver, Color.Gold, Color.Silver, Color.Silver, Color.Silver, Color.Brown};
             for (int i = 0, x = 100, y = 100; i < currVals.GetLength(1); ++i)
             {
@@ -108,14 +108,14 @@ namespace RylandHortonReviewLab
                     if (i <= 3)
                     {
                         window.AddRectangle(x, y, 180, 80, colours[i], 1, Color.Beige);
-                        window.AddText("$" + ((currVals[0,i] == 0.1) ? "0.10" : currVals[0,i].ToString()) + " x " + currVals[1, i].ToString(), 14, x + 90, y + 40, 0, 0, Color.Black);
+                        window.AddText("$" + ((currVals[0,i] == 0.1m) ? "0.10" : currVals[0,i].ToString()) + " x " + currVals[1, i].ToString(), 14, x + 90, y + 40, 0, 0, Color.Black);
                         y += 90;
                     }
                     else
                     {
                         window.AddEllipse(x + 45, y, 85, 85, colours[i], 1, Color.Beige);
                         if (currVals[0, i] == 2) window.AddEllipse(x + 63, y+20, 47, 47, Color.Gold);
-                        window.AddText("$" + ((currVals[0,i] == 0.1) ? "0.10" : currVals[0,i].ToString()) + " x " + currVals[1, i].ToString(), 14, x + 85, y + 42, 0, 0, Color.Black);
+                        window.AddText("$" + ((currVals[0,i] == 0.1m) ? "0.10" : currVals[0,i].ToString()) + " x " + currVals[1, i].ToString(), 14, x + 85, y + 42, 0, 0, Color.Black);
                         y += 90;
                     }
                     
